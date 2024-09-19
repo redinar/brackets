fba098713acde147c8d61ce0357aab2cb81672e8
Dinar <163840209+redinar@users.noreply.github.com>
2024-09-19 16:29:23 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack =[];
+  openingBrackets = ['(', '[', '{', '|'];
+  closingBrackets = {
+    [')']: '(',
+    [']']: '[',
+    ['}']: '{',
+    ['|']: '|'
+  }
+  for (let i=0; i<str.length; i++){
+    if (openingBrackets.includes(str[i])) {
+      stack.push(str[i]);
+    } else if (stack.length === 0) return false;
+    
+    if (stack[stack.length-1] === closingBrackets[str[i]]) {
+      stack.pop();
+    }
+  }
+
+  if (stack.length === 0) return true
+  else return false;
 }
